10fb0193423f8a3da6c2749a5fd3a60a41305ddc
txtyb <txtybbill@163.com>
2022-06-22 03:20:34 +0800

    Add data verification and type check

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
+from datetime import date
+from certifi import contents
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os, requests
@@ -11,6 +13,12 @@ cors = CORS(app)
 
 @app.route('/api/getDevice', methods=['GET'])
 def getDevice():
+    def isDataValid(payloadData):
+        if (len(payloadData) == 18) and (payloadData[0:2] == 'AA') and (payloadData[-2:] == '0F'):
+            return True
+        else:
+            return False
+
     url = 'http://' + SCSX_HOST + '/api/v1/lorawan/datas/devices'
 
     n = request.args.get('n', type=int, default=10)
@@ -29,9 +37,29 @@ def getDevice():
     response = requests.get(url=url, headers=headers, params=params)
 
     resDate = response.json()
+    if resDate['code'] == 9001:
+        return 'Invalid authentication token', 401
+    elif resDate['msg'] == '请求成功':
+        # reverse to make the last the latest
+        content = resDate['data']['content'][::-1]
+        dateUpList = list()
+        dateType = ['光照', '气压', '温度', '湿度']
+        for i in content:
+            if i['mtypeText'] == 'CONFIRMED_DATA_UP':
+                payloadData = i['payloadData']
+                if isDataValid(payloadData):
+                    dataUp = dict()
+                    dataUp['type'] = dateType[int(payloadData[7])-1]
+                    dataUp['timestamp'] = i['serverTimeMillis']
+                    dataUp['time'] = i['serverTime']
+                    dataUp['data'] = i['payloadData']
+                    hexStr1 = '0x' + payloadData[10:14]
+                    hexStr2 = '0x' + payloadData[14:16]
+                    dataUp['value'] = float.fromhex(hexStr1) + (float.fromhex(hexStr2)*0.01)
 
-    if resDate['msg'] == '请求成功':
-        return jsonify(resDate['data']['content'])
+                    dateUpList.append(dataUp)
+
+        return jsonify(dateUpList)
 
 if __name__ == '__main__':
     app.run()
